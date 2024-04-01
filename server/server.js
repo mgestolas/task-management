ada2975ccb87d14c7dd9650a1fcc89660e0edb5a
cesarpizarra <cpizarra002@gmail.com>
2024-04-01 13:49:41 +0800

    update allow url

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://task-management-three-green.vercel.app/",
+      "https://task-management-three-green.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
